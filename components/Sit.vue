3c9e079c53bd4d92ca18da05646bd43150a3a211
Jimmy Liao <jimmy.liao@gogoro.com>
2025-03-17 16:14:44 +0800

    Fix public assets

--- components/Sit.vue
+++ components/Sit.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex items-center justify-center mt-16">
-    <img src="./images/Sit.webp" width="300px" />
+    <img src="/images/Sit.webp" width="300px" />
   </div>
 </template>
